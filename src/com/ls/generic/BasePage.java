0288c2132d488f9a3e028055fae9ccc3cae40ee1
Shobhan K S <shobhan.ks@testyantra.com>
2019-06-07 12:12:05 +0530

    2nd commit

--- src/com/ls/generic/BasePage.java
+++ src/com/ls/generic/BasePage.java
@@ -1,21 +1,26 @@
 package com.ls.generic;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class BasePage {
 
-	 public WebDriver driver;
+	public WebDriver driver;
 	WebDriverWait wait;
-
 
 	public BasePage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	// To click on the required element
 	public void clickElement(WebElement element) {
 		element.click();
@@ -25,6 +30,34 @@ public class BasePage {
 	public void typeText(WebElement element, String text) {
 		element.sendKeys(text);
 	}
-	
-	
+
+	// #################################################################//
+	// Selenium methods
+	public void waitForPageLoaded() {
+		ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString()
+						.equals("complete");
+			}
+		};
+		try {
+			Thread.sleep(1000);
+			WebDriverWait wait = new WebDriverWait(driver, 5);
+			wait.until(expectation);
+		} catch (Throwable error) {
+			Assert.fail("Timeout waiting for Page Load Request to complete.");
+		}
+	}
+
+	public void waitUntilLoadedAndVisibilityOfElementLocated(WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	public void mouseHover(WebElement element) {
+		Actions action = new Actions(driver);
+		action.moveToElement(element);
+
+	}
+
 }
--- src/com/ls/pom/LoginPage.java
+++ src/com/ls/pom/LoginPage.java
@@ -8,32 +8,31 @@ import com.ls.generic.BasePage;
 
 public class LoginPage extends BasePage {
 
-
-	//By userNameTB = By.xpath("//input[@id='__xmlview1--userName-inner']");
-	// passwordTB = By.xpath("//input[@id='__xmlview1--password-inner']");
-	//By clkDropdown = By.xpath("//span[@id='__xmlview1--site-arrow']");
-	//By clkTSK = By.xpath("//*[@id='__item4'][text()='TSK']");
-	//By clkLogin = By.xpath("//bdi[text()='Log In']");
-
-	
-	@FindBy(id="EmailID")
+	@FindBy(id = "EmailID")
 	private WebElement userNameTxtBx;
-	
-	@FindBy(id="Password")
+
+	@FindBy(id = "Password")
 	private WebElement passwordTxtBx;
 
-	@FindBy(id="login")
+	@FindBy(id = "login")
 	private WebElement loginBtn;
 
-
 	public LoginPage(WebDriver driver) {
-      super(driver);
+		super(driver);
 	}
-	
+
+	/**
+	 * @author ShobhanKS
+	 * @description Logging to the Application 
+	 * @param username
+	 * @param password
+	 */
 	public void testLogin(String username, String password) {
-    typeText(userNameTxtBx, username);
-    typeText(passwordTxtBx, password);
-    clickElement(loginBtn);
+		
+		System.out.println("Login to the Application with valid credentails");
+		typeText(userNameTxtBx, username);
+		typeText(passwordTxtBx, password);
+		clickElement(loginBtn);
 	}
 
 }

ca6c72e9e4a7eddf10d6184bbd3b884c4546653c
Shobhan K S <shobhan.ks@testyantra.com>
2019-06-11 12:29:57 +0530

    user

---
+++ src/com/ls/pom/settings/usersandpermissions/UsersPage.java
@@ -0,0 +1,43 @@
+package com.ls.pom.settings.usersandpermissions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.ls.generic.BasePage;
+
+public class UsersPage extends BasePage {
+
+	public UsersPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(xpath="")
+	private WebElement userLnk;
+	
+	@FindBy(xpath="")
+	private WebElement createBtn;
+	
+	@FindBy(id="FirstName")
+	private WebElement firstNameTxtBx;
+	
+	@FindBy(id="LastName")
+	private WebElement lastNameTxtBx;
+	
+	@FindBy(id="EmailAddress")
+	private WebElement emailAddressTxtBx;
+	
+	@FindBy(xpath="//input[@class='ui-widget-content ui-autocomplete-input']")
+	private WebElement phoneTxtBx;
+	
+	@FindBy(xpath="//span[@class='custom-combobox']")
+	private WebElement reportingToTxtBx;
+	
+	
+	@FindBy(xpath="//button[.='Create User']")
+	private WebElement createUserBtn;
+		
+	public void clkCreateUser() {
+		clickElement(createBtn);	
+	}
+}
